84bb53bee9eb4f28418c3df29a0b3f3df471b906
markocodes <Marko.Majkic@outlook.com>
2022-02-09 22:35:42 -0500

    Created logging class with logging features

---
+++ src/Logger.py
@@ -0,0 +1,37 @@
+class Logger:
+    __enable_inspector_logging = True
+    __enable_workstation_logging = True
+    __create_log_files = True
+
+    def __init__(self, enable_inspector_logging, enable_workstation_logging, create_log_files):
+        self.__enable_inspector_logging = enable_inspector_logging
+        self.__enable_workstation_logging = enable_workstation_logging
+        self.__create_log_files = create_log_files
+
+    def log_inspector_component_selection(component, time):
+        if Logger.__enable_inspector_logging == True:
+            print("The inspector has started inspecting " + component.name + " at " + time + "s")
+        else:
+            return
+
+    def log_inspector_buffered_component(component, workstation, time):
+        if Logger.__enable_inspector_logging == True:
+            print("The inspector has placed" + component.name + " in " + workstation.name + "'s buffer at " + time + "s")
+        else:
+            return
+
+    def log_workstation_unbuffer(workstation, component, time):
+        if Logger.__enable_workstation_logging == True:
+            print(component.name + " has entered " + workstation.name + " at time " + time + "s")
+        else:
+            return
+    
+    def log_product_created(product, time):
+        if Logger.__enable_workstation_logging == True:
+            print(product.name + "has been completed at " + time + "s")
+        else:
+            return
+
+
+
+
