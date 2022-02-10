14b7c01858c4759dffe4f7bd572a61e208486fe8
Millan <real.millan.wang@gmail.com>
2022-02-10 12:15:54 -0500

    Added buffer. Still WorkInProg

--- src/SimulationEnums.py
+++ src/SimulationEnums.py
@@ -11,3 +11,10 @@ class Component(enum.Enum):
     C1 = 1
     C2 = 2
     C3 = 3
+
+
+class Event_Types(enum.Enum):
+    Inspection_Complete = 1
+    Add_to_Buffer = 2
+    Unbuffer_Start_Assembly = 3
+    Assembly_Complete = 4
