024eb5d0b5ff4b023c1d91694aa07f966fb9d0e0
Millan <real.millan.wang@gmail.com>
2022-02-10 21:21:55 -0500

    Minor workstation and inspector updates

--- src/Inspectors.py
+++ src/Inspectors.py
@@ -6,18 +6,17 @@ from SimulationEnums import Component
 
 class Inspector1(object):
     """
-    Class for Inspector1
+        Class for Inspector1
     """
     def __init__(self):
         """
-        intilazation of the Inspector1 class 
+            intilazation of the Inspector1 class 
         """
-        __mean = 0
+        self.__mean= self.__build_distribution_from_dat()
 
-
-    def mean(self):
+    def __build_distribution_from_dat(self):
         """
-        Calculaltes the Mean for component 1 from the data set. returns the mean
+            Calculaltes the Mean for component 1 from the data set. returns the mean
         """
         script_dir = os.path.dirname(__file__) #this is to access the data files in their folder 
         rel_path = "dat_files/servinsp1.dat"
@@ -25,51 +24,45 @@ class Inspector1(object):
         self.datalist = open(abs_file_path).read().splitlines()
         datatotal = 0
         for x in range(0, 300):
-            datatotal += float(self.datalist[x])# Reads all of the data points and sums them up
+            datatotal += float(self.datalist[x]) # Reads all of the data points and sums them up
         mean = datatotal / 300
         return mean
     
-    def __generate_comp1(self):
+    def generate_inspect_time(self):
         """
-        function to create component 1s returns component 1
+            Creates a inspection time delay from the distribution returns delay time and component 
         """
-        return Component.C1
-    
-    def generate_inspect_time(component, mean):
-        """
-        Creates a inspection time delay from the distribution returns delay time and component 
-        """
-        time = numpy.random.exponential(mean, 1) #Generate the random time delay from the mean
+        time = numpy.random.exponential(self.__mean, 1) #Generate the random time delay from the mean
         time = int("".join(time.split("."))) # change to seconds
-        return time, component
+        return time, Component.C1
 
 
 
 
 class Inspector2(object):
     """
-    Class for Inspector2
+        Class for Inspector2
     """
     
     def __init__(self):
         """
-        intilazation of the Inspector1 class 
+            intilazation of the Inspector1 class 
         """
-        self.__C2_mean = self.mean(Component.C2)
-        self.__C3_mean = self.mean(Component.C3)
+        self.__C2_mean = self.__build_distribution_from_dat(Component.C2)
+        self.__C3_mean = self.__build_distribution_from_dat(Component.C3)
     
     def __generate_comp2or3(self):
         """
-        Randomly picks between component 2 and 3 returns the component
-         """
+            Randomly picks between component 2 and 3 returns the component
+        """
         if random.getrandbits(1) == 1:
             return Component.C2
         else:
             return Component.C3
     
-    def mean(self, component):
+    def __build_distribution_from_dat(self, component):
         """
-        Calculaltes the Mean for component 1 from the distribution  returns the mean  of the data set
+            Calculaltes the Mean for component 1 from the distribution  returns the mean  of the data set
         """
         if component == Component.C2:
             rel_path = "dat_files/servinsp22.dat"
@@ -86,8 +79,8 @@ class Inspector2(object):
     
     def generate_inspect_time(self):
         """
-        Creates a inspection time delay from the distribution 
-        returns the delay time and the component 
+            Creates a inspection time delay from the distribution 
+            returns the delay time and the component 
         """
         component =  self.__generate_comp2or3()
         if component == Component.C2:
