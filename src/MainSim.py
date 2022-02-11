aff84a6d9d1762e6dbb0d90b6ac993c7ce465c5c
Millan <real.millan.wang@gmail.com>
2022-02-10 20:18:52 -0500

    updates to sim and buffer

--- src/MainSim.py
+++ src/MainSim.py
@@ -1,5 +1,6 @@
 import numpy as np
 import queue
+from Inspectors import Inspector1, Inspector2
 from SimulationEnums import Component, Product, Event_Types
 from SimulationLogger import SimulationLogger
 from Buffers import Component_Buffer_Manager
@@ -12,7 +13,7 @@ BUFFER_CAPACITY = 2
 
 
 class Simulation(object):
-    def __init__(self, simulation_logger):
+    def __init__(self, simulation_logger: SimulationLogger):
         self._clock = 0
         self._simulation_logger = simulation_logger
 
@@ -20,16 +21,46 @@ class Simulation(object):
 
         # [Total, P1, P2, P3] - index matches product
         self._product_counts = [0, 0, 0, 0]
+
+        # [None, I1, I2] so inspector number matches index
+        self._inspectors = [None, Inspector1(), Inspector2()]
+
+        # [None, W1, W2, W3] so workstation number match index
+        self._workstations = [None]  # WORKSTATIONS NEEDED
+
         self._buffer_manager = Component_Buffer_Manager()
 
-    def schedule_add_to_buffer(self, inspector):
-        return inspector
-
-    def process_add_to_buffer(self):
+    def schedule_add_to_buffer(self, inspector_number: int):
+        # inspect_time, component = self._inspectors[inspector_number].get_inspect_time()
+        inspect_time, component = 123
         return 0
 
-    def process_workstation_unbuffer(self):
+    def process_add_to_buffer(self, component: Component):
+        success, product = self._buffer_manager.attempt_to_add_to_buffer(
+            component)
+
+        if not success:
+            # Inspector is blocked and can't add to buffer right now
+            return
         return 0
+
+    def process_workstation_unbuffer(self, product: Product):
+        success = self._buffer_manager.attempt_to_assemble_product(product)
+        if not success:
+            # Could not build the product. Missing items on buffer
+            return
+
+        workstation_assembly_time = 123
+        # workstation_assembly_time = self._workstations[product.value].get_assembly_time()
+        return 0
+
+    def process_product_made(self, product: Product):
+        """
+            Processes the process of completeing the production of a product
+        """
+        self._product_counts[0] += 1  # Total product counter
+        self._product_counts[product.value] += 1  # Product specific counter
+        self._simulation_logger.log_product_created(product, self._clock)
 
 
 # Main script
@@ -45,6 +76,8 @@ if __name__ == "__main__":
 
     # Create simulation object
     sim = Simulation(sim_logger)
+
+    # Schedule first inspection completion for both inspectors
 
     while sim._product_counts[0] <= PRODUCT_CREATION_LIMIT:
         # Events (time, event_type, )
