beb63cf45553141896edd79c1cd4c66b82875942
Millan <real.millan.wang@gmail.com>
2022-03-11 03:19:23 -0500

    Added WIP Chi squared

--- src/Stat_Manger.py
+++ src/Stat_Manger.py
@@ -1,8 +1,9 @@
+import math
 import numpy
 import os
 import random
 from Grapher import Grapher
-
+import numpy as np
 
 class Stat_Manager(object):
 
@@ -54,7 +55,7 @@ class Stat_Manager(object):
         #print(intlist)
     
 
-        return (intlist,qq_list, mean, variance, count, sum)
+        return (intlist,qq_list, mean, variance, count, sample_sum)
 
 
     def qq_plot(self):
@@ -72,25 +73,62 @@ class Stat_Manager(object):
         Send required Data to plot the Histogram to the Graph_Master class 
         """
         Grapher.build_histogram(self.__dat_file_contents[0], "Histogram")
-    def chi_squared(self):
+
+    def exponential_chi_squared(self):
         """
         Calculate the Chi Squared Value
         For each time in the list call the random generator of that 
         distribution to get a second value. each set of values  
         
         """
-        observed = self.__dat_file_contents[0]
+        # (intlist,qq_list, mean, variance, count, sample_sum)
+        # self.__dat_file_contents
+        sorted_data = self.__dat_file_contents[1]
+        print(sorted_data[-1])
+        # Calculating number of bins and bin width
+        q25, q75 = np.percentile(sorted_data, [25, 75])
+        bin_width = 2 * (q75 - q25) * len(sorted_data) ** (-1/3)
+        k_intervals = round((max(sorted_data) - min(sorted_data)) / bin_width)
 
-        estimated = [6.25,6.25,6.25,6.25,6.25,6.25,6.25,6.25]
-        chiSQpart = []
-        for i in self.__dat_file_contents[0]:
-            chiSQpart.append((((observed[i]-estimated[i])**2)/estimated[i]))
-        chiSQtotal= sum(chiSQpart)
-        print(chiSQpart)
-        print(chiSQtotal)
+        exponential_lambda = 2
 
-        return
+        expected_frequencies = []
+        for i in range(k_intervals+1):
+            expected_frequencies.append((exponential_cdf(i+1,exponential_lambda)-exponential_cdf(i,exponential_lambda))*300)
+            
+        observed_frequencies = []
+        for i in range(k_intervals-1):
+            observed_frequencies.append(0)
+            for elem in sorted_data:
+                if elem>(i)*bin_width and elem<=(i+1)*bin_width:
+                    observed_frequencies[i]+=1
 
-    def parameter_estimation():
-        return
-x= Stat_Manager(1)
+        #Last element in the list is sometimes wonky. It's sorted so add it to last bin
+        if len(observed_frequencies)<len(sorted_data) : observed_frequencies[-1]+=1
+        chi_components = []
+        for i in range(len(observed_frequencies)):
+            chi_components.append( math.pow(observed_frequencies[i]-expected_frequencies[i],2) / expected_frequencies[i])
+        
+
+        # All dat files in this project have 300 datapoints. 
+        chi_squared_table_value = 339.26047583
+        if chi_squared_table_value >sum(chi_components):
+            print("Null hypothesis NOT rejected - Can assume distributions match")
+        else:
+            print("Null hypothesis REJECTED - Distributions don't match")
+        print(sum(chi_components))
+        print(observed_frequencies)
+        print(expected_frequencies)
+        print(sum(observed_frequencies))
+        print(sum(expected_frequencies))
+
+
+
+
+def weibull_cdf(x,k,b):
+    return 1 - math.exp(-b*math.pow(x,k))
+
+
+
+def exponential_cdf(x,ld):
+    return 1 - math.exp(-x*ld)
