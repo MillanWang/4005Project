265237e0d55ec38e87f5d12223d2c35d1662d706
Millan <real.millan.wang@gmail.com>
2022-04-10 01:12:54 -0400

    Updated sim stat generation and added a skeleton for alt operating method

--- src/Buffers.py
+++ src/Buffers.py
@@ -4,19 +4,19 @@ BUFFER_CAPACITY = 2
 
 class Component_Buffer(object):
     def __init__(self):
-        self._component_count = 0
+        self.component_count = 0
 
     def attempt_to_add_to_buffer(self):
         """
             Attempts to add to the current buffer
             returns True if successful, False if buffer is full
         """
-        if (self._component_count >= BUFFER_CAPACITY):
+        if (self.component_count >= BUFFER_CAPACITY):
             # Buffer is full. Cannot add to it
             return False
         else:
             # Buffer has space. Add to it
-            self._component_count += 1
+            self.component_count += 1
             return True
 
     def remove_from_buffer(self):
@@ -24,20 +24,20 @@ class Component_Buffer(object):
             Removes component from buffer. 
             Throws error if buffer is empty
         """
-        if (self._component_count <= 0):
+        if (self.component_count <= 0):
             raise ValueError("Attempted to remove component from empty buffer")
-        self._component_count -= 1
+        self.component_count -= 1
 
 
 class Component_Buffer_Manager(object):
     def __init__(self):
         # Create and populate buffer dictionary
-        self._buffer_dict = dict()
-        self._buffer_dict[(Component.C1, Product.P1)] = Component_Buffer()
-        self._buffer_dict[(Component.C1, Product.P2)] = Component_Buffer()
-        self._buffer_dict[(Component.C2, Product.P2)] = Component_Buffer()
-        self._buffer_dict[(Component.C1, Product.P3)] = Component_Buffer()
-        self._buffer_dict[(Component.C3, Product.P3)] = Component_Buffer()
+        self.__buffer_dict = dict()
+        self.__buffer_dict[(Component.C1, Product.P1)] = Component_Buffer()
+        self.__buffer_dict[(Component.C1, Product.P2)] = Component_Buffer()
+        self.__buffer_dict[(Component.C2, Product.P2)] = Component_Buffer()
+        self.__buffer_dict[(Component.C1, Product.P3)] = Component_Buffer()
+        self.__buffer_dict[(Component.C3, Product.P3)] = Component_Buffer()
 
     def attempt_to_add_to_buffer(self, component: Component):
         """
@@ -48,36 +48,36 @@ class Component_Buffer_Manager(object):
         if component == Component.C1:
             # Check capacities of all C1 buffers [P1,P2,P3] buffer capacity list
             buffer_capacities = []
-            buffer_capacities.append(self._buffer_dict[(Component.C1, Product.P1)]._component_count)
-            buffer_capacities.append(self._buffer_dict[(Component.C1, Product.P2)]._component_count)
-            buffer_capacities.append(self._buffer_dict[(Component.C1, Product.P3)]._component_count)
+            buffer_capacities.append(self.__buffer_dict[(Component.C1, Product.P1)].component_count)
+            buffer_capacities.append(self.__buffer_dict[(Component.C1, Product.P2)].component_count)
+            buffer_capacities.append(self.__buffer_dict[(Component.C1, Product.P3)].component_count)
 
             # Check if all buffers are full. Can't add if they are
             if sum(buffer_capacities) == BUFFER_CAPACITY * 3:
                 return False, None
 
-            # Get the index of the lowest capacity. First index if tied
+            # Get the index of the lowest capacity. First/lowest index if tied
             # P1 before P2 before P3 prioritization
             buffer_choice = buffer_capacities.index(min(buffer_capacities))
 
             if buffer_choice == 0:
-                return self._buffer_dict[(Component.C1, Product.P1)].attempt_to_add_to_buffer(), Product.P1
+                return self.__buffer_dict[(Component.C1, Product.P1)].attempt_to_add_to_buffer(), Product.P1
             elif buffer_choice == 1:
-                return self._buffer_dict[(Component.C1, Product.P2)].attempt_to_add_to_buffer(), Product.P2
+                return self.__buffer_dict[(Component.C1, Product.P2)].attempt_to_add_to_buffer(), Product.P2
             elif buffer_choice == 2:
-                return self._buffer_dict[(Component.C1, Product.P3)].attempt_to_add_to_buffer(), Product.P3
+                return self.__buffer_dict[(Component.C1, Product.P3)].attempt_to_add_to_buffer(), Product.P3
 
             # Should never get here. Error if we do
             raise ValueError("Internal buffer manager error attempting to add component C1 to a buffer")
 
         elif component == Component.C2:
             # Only one possible buffer this component can be sent to
-            add_success = self._buffer_dict[(Component.C2, Product.P2)].attempt_to_add_to_buffer()
+            add_success = self.__buffer_dict[(Component.C2, Product.P2)].attempt_to_add_to_buffer()
             return add_success,  Product.P2 if add_success else None
 
         elif component == Component.C3:
             # Only one possible buffer this component can be sent to
-            add_success = self._buffer_dict[(Component.C3, Product.P3)].attempt_to_add_to_buffer()
+            add_success = self.__buffer_dict[(Component.C3, Product.P3)].attempt_to_add_to_buffer()
             return add_success,  Product.P3 if add_success else None
         # END Component_Buffer_Manager.attempt_to_add_to_buffer
 
@@ -90,18 +90,29 @@ class Component_Buffer_Manager(object):
             Returns False if there are missing components on the buffers
         """
         if product == Product.P1:
-            if (self._buffer_dict[(Component.C1, Product.P1)]._component_count == 0):
+            if (self.__buffer_dict[(Component.C1, Product.P1)].component_count == 0):
                 return False
-            self._buffer_dict[(Component.C1, Product.P1)].remove_from_buffer()
+            self.__buffer_dict[(Component.C1, Product.P1)].remove_from_buffer()
         elif product == Product.P2:
-            if (self._buffer_dict[(Component.C1, Product.P2)]._component_count == 0 or self._buffer_dict[(Component.C2, Product.P2)]._component_count == 0):
+            if (self.__buffer_dict[(Component.C1, Product.P2)].component_count == 0 or self.__buffer_dict[(Component.C2, Product.P2)].component_count == 0):
                 return False
-            self._buffer_dict[(Component.C1, Product.P2)].remove_from_buffer()
-            self._buffer_dict[(Component.C2, Product.P2)].remove_from_buffer()
+            self.__buffer_dict[(Component.C1, Product.P2)].remove_from_buffer()
+            self.__buffer_dict[(Component.C2, Product.P2)].remove_from_buffer()
         elif product == Product.P3:
-            if (self._buffer_dict[(Component.C1, Product.P3)]._component_count == 0 or self._buffer_dict[(Component.C3, Product.P3)]._component_count == 0):
+            if (self.__buffer_dict[(Component.C1, Product.P3)].component_count == 0 or self.__buffer_dict[(Component.C3, Product.P3)].component_count == 0):
                 return False
-            self._buffer_dict[(Component.C1, Product.P3)].remove_from_buffer()
-            self._buffer_dict[(Component.C3, Product.P3)].remove_from_buffer()
+            self.__buffer_dict[(Component.C1, Product.P3)].remove_from_buffer()
+            self.__buffer_dict[(Component.C3, Product.P3)].remove_from_buffer()
 
         return True  # Removed needed components from buffer
+    
+    def choose_next_inspector2_component(self):
+        """
+            Returns the next component that inspector 2 should inspect based on the current buffer capacities
+        """
+        if self.__buffer_dict[(Component.C2, Product.P2)].component_count < self.__buffer_dict[(Component.C3, Product.P3)].component_count:
+            return Component.C2
+        elif self.__buffer_dict[(Component.C2, Product.P2)].component_count > self.__buffer_dict[(Component.C3, Product.P3)].component_count:
+            return Component.C3
+        else:
+            return None
--- src/MainSim.py
+++ src/MainSim.py
@@ -15,39 +15,41 @@ ENABLE_INSPECTOR_LOGGING = False  # or True
 ENABLE_WORKSTATION_LOGGING = False  # or True
 CREATE_LOG_FILES = False  # or True
 
-TOTAL_PRODUCT_CREATION_LIMIT = 1000
+TOTAL_PRODUCT_CREATION_LIMIT = 1000*50
+SIMULATION_REPLICATION_COUNT = 500
 
 
 
 class Simulation(object):
     def __init__(self, simulation_logger: SimulationLogger, seed : int):
-        self._clock = 0
-        self._simulation_logger = simulation_logger
+        self.clock = 0
+        self.__simulation_logger = simulation_logger
 
         #Using a list instead of a queue because the order that events occur may not be chronological
-        self._future_event_list = []
-
-        
+        self.__future_event_list = []
 
         randogen = Lecuyer_Generator(seed)
 
         # [None, I1, I2] so inspector number matches index
-        self._inspectors = [None, Inspector1(randogen), Inspector2(randogen)]
+        self.__inspectors = [None, Inspector1(randogen), Inspector2(randogen)]
 
         # [None, W1, W2, W3] so workstation number match index
-        self._workstations = [None, 
+        self.__workstations = [None, 
                               Workstation(Product.P1,randogen),
                               Workstation(Product.P2,randogen),
                               Workstation(Product.P3,randogen)] 
 
-        self._buffer_manager = Component_Buffer_Manager()
+        self.__buffer_manager = Component_Buffer_Manager()
 
         # [Total, P1, P2, P3] - index matches product
-        self._product_counts = [0, 0, 0, 0]
-        self._component_most_recent_block_time = [None, -1, -1, -1]
-        self._component_total_block_time = [None, 0,0,0]
-        self._workstation_most_recent_wait_for_component_time = [None, 0,0,0]
-        self._workstation_total_wait_for_component_time = [None, 0,0,0]
+        self.product_counts = [0, 0, 0, 0]
+        self.__component_most_recent_block_time = [None, -1, -1, -1]
+        self.__component_total_block_time = [None, 0,0,0]
+        self.__component_block_counts = [None, 0,0,0]
+        self.__component_inspection_counts = [None, 0,0,0]
+        self.__workstation_most_recent_wait_for_component_time = [None, 0,0,0]
+        self.__workstation_total_wait_for_component_time = [None, 0,0,0]
+        self.__workstation_wait_counts = [None, 0,0,0]
 
     
 
@@ -55,19 +57,19 @@ class Simulation(object):
         """
             Removes and returns the next nearest chronological event from the future event list 
         """
-        if len(self._future_event_list) == 0:
+        if len(self.__future_event_list) == 0:
             raise ValueError("Error trying to get next chronological event before any have been scheduled")
         
         # Identify the index of the earliest chronological event from the future event list
-        current_min = self._future_event_list[0][0]
+        current_min = self.__future_event_list[0][0]
         current_min_index = 0
 
-        for i in range(len(self._future_event_list)):
-            if self._future_event_list[i][0] < current_min:
-                current_min = self._future_event_list[i][0]
+        for i in range(len(self.__future_event_list)):
+            if self.__future_event_list[i][0] < current_min:
+                current_min = self.__future_event_list[i][0]
                 current_min_index = i
         # Remove and return the event at that index
-        return self._future_event_list.pop(current_min_index)
+        return self.__future_event_list.pop(current_min_index)
 
 
 
@@ -76,12 +78,15 @@ class Simulation(object):
             Initiates an inspector's inspection process and schedules 
             the attempt to add the component to the buffer
         """
-        inspect_time, component = self._inspectors[inspector_number].generate_inspect_time()
-        self._simulation_logger.log_inspector_component_selection(component, self._clock)
+        inspect_time, component = self.__inspectors[inspector_number].generate_inspect_time()
+        self.__simulation_logger.log_inspector_component_selection(component, self.clock)
+
+        # Note that the component is being  inspected
+        self.__component_inspection_counts[component.value] += 1
 
         #Schedule event for inspection completion
-        completion_time = self._clock + inspect_time
-        self._future_event_list.append((completion_time, Event_Types.Inspection_Complete, component))
+        completion_time = self.clock + inspect_time
+        self.__future_event_list.append((completion_time, Event_Types.Inspection_Complete, component))
 
 
 
@@ -90,20 +95,23 @@ class Simulation(object):
             Processes attempting to add an inspected component to a buffer
         """
         # If successful, the buffers will be updated accordingly by calling this
-        success, product = self._buffer_manager.attempt_to_add_to_buffer(component)
+        success, product = self.__buffer_manager.attempt_to_add_to_buffer(component)
+
+
 
         if not success: 
             # Inspector is blocked and can't add to buffer right now
             # Set the current time as the most recent block time
-            self._component_most_recent_block_time[component.value] = self._clock
+            self.__component_most_recent_block_time[component.value] = self.clock
+            self.__component_block_counts[component.value] += 1
             return
 
         # If successfully put component onto buffer, start to inspect again A$AP
-        self._future_event_list.append((self._clock, Event_Types.Start_Next_Inspection, 1 if component==Component.C1 else 2))
+        self.__future_event_list.append((self.clock, Event_Types.Start_Next_Inspection, 1 if component==Component.C1 else 2))
 
         # Schedule event for adding to buffer A$AP
-        self._simulation_logger.log_inspector_buffered_component(component, product, self._clock)
-        self._future_event_list.append((self._clock, Event_Types.Add_to_Buffer, product))
+        self.__simulation_logger.log_inspector_buffered_component(component, product, self.clock)
+        self.__future_event_list.append((self.clock, Event_Types.Add_to_Buffer, product))
 
 
 
@@ -114,10 +122,10 @@ class Simulation(object):
             Processes attempting to start assembling a product
         """
         # Check if the current workstation is busy
-        if self._workstations[product.value].is_building():
+        if self.__workstations[product.value].is_building():
             return
 
-        success = self._buffer_manager.attempt_to_assemble_product(product)
+        success = self.__buffer_manager.attempt_to_assemble_product(product)
         if not success:
             # Could not build the product. 1 or more missing items on workstation buffer
             # Will eventually be successful once buffers get occupied
@@ -125,40 +133,45 @@ class Simulation(object):
         # Successfully unbuffered
 
         # Check if inspector 2 is waiting for a buffer spot 
-        if product!=Product.P1 and self._component_most_recent_block_time[product.value]!=-1:
+        if product!=Product.P1 and self.__component_most_recent_block_time[product.value]!=-1:
             # Component 2 or 3 get's buffered, resume Inspector 2
-            self._buffer_manager.attempt_to_add_to_buffer(Component.C2 if product==Product.P2 else Component.C3)
+            self.__buffer_manager.attempt_to_add_to_buffer(Component.C2 if product==Product.P2 else Component.C3)
 
             # Track the blocked time
-            blocked_time = self._clock - self._component_most_recent_block_time[product.value]
-            self._component_total_block_time[product.value] += blocked_time
+            blocked_time = self.clock - self.__component_most_recent_block_time[product.value]
+            self.__component_total_block_time[product.value] += blocked_time
             # Reset the most recent block time to unblocked
-            self._component_most_recent_block_time[product.value]!=-1
+            self.__component_most_recent_block_time[product.value]!=-1
+
             # Start inspector 2 next inspection A$AP
-            self._future_event_list.append((self._clock, Event_Types.Start_Next_Inspection, 2))
+            self.__future_event_list.append((self.clock, Event_Types.Start_Next_Inspection, 2))
         
         # Check if inspector 1 is waiting for a buffer spot 
-        if self._component_most_recent_block_time[1]!=-1:
+        if self.__component_most_recent_block_time[1]!=-1:
             # All products use component 1. Add it to the buffer if inspector 1 is stuck. Resume inspector 1
-            self._buffer_manager.attempt_to_add_to_buffer(Component.C1)
+            self.__buffer_manager.attempt_to_add_to_buffer(Component.C1)
+            
             # Track the blocked time
-            blocked_time = self._clock - self._component_most_recent_block_time[1]
-            self._component_total_block_time[1] += blocked_time
+            blocked_time = self.clock - self.__component_most_recent_block_time[1]
+            self.__component_total_block_time[1] += blocked_time
             # Reset the most recent block time to unblocked
-            self._component_most_recent_block_time[1]!=-1
+            self.__component_most_recent_block_time[1]!=-1
+
             # Start the next inspection A$AP
-            self._future_event_list.append((self._clock, Event_Types.Start_Next_Inspection, 1 ))
+            self.__future_event_list.append((self.clock, Event_Types.Start_Next_Inspection, 1 ))
+
+            
 
         # Get the time that the workstation has waiting to start building
-        waiting_time = self._clock - self._workstation_most_recent_wait_for_component_time[product.value]
-        self._workstation_total_wait_for_component_time[product.value] += waiting_time
+        waiting_time = self.clock - self.__workstation_most_recent_wait_for_component_time[product.value]
+        self.__workstation_total_wait_for_component_time[product.value] += waiting_time
         
-        workstation_assembly_time = self._workstations[product.value].get_product_build_time()
-        self._simulation_logger.log_workstation_unbuffer(product, self._clock)
+        workstation_assembly_time = self.__workstations[product.value].get_product_build_time()
+        self.__simulation_logger.log_workstation_unbuffer(product, self.clock)
         
         # Schedule event for completing assembly
-        build_completion_time = self._clock + workstation_assembly_time
-        self._future_event_list.append((build_completion_time, Event_Types.Assembly_Complete, product))
+        build_completion_time = self.clock + workstation_assembly_time
+        self.__future_event_list.append((build_completion_time, Event_Types.Assembly_Complete, product))
 
 
 
@@ -166,37 +179,65 @@ class Simulation(object):
         """
             Processes the completeing the a specified product's assembly
         """
-        self._product_counts[0] += 1  # Total product counter
-        self._product_counts[product.value] += 1  # Product specific counter
-        self._simulation_logger.log_product_created(product, self._clock)
+        self.product_counts[0] += 1  # Total product counter
+        self.product_counts[product.value] += 1  # Product specific counter
+        self.__simulation_logger.log_product_created(product, self.clock)
 
-        self._workstations[product.value].complete_build()
+        self.__workstations[product.value].complete_build()
 
         # Try to create another product and note time to for waiting for component
-        self._future_event_list.append((self._clock, Event_Types.Unbuffer_Start_Assembly, product))
-        self._workstation_most_recent_wait_for_component_time[product.value] = self._clock
+        self.__future_event_list.append((self.clock, Event_Types.Unbuffer_Start_Assembly, product))
+        self.__workstation_most_recent_wait_for_component_time[product.value] = self.clock
 
 
-    def print_sim_summary(self):
+    def print_sim_summary(self, sim_number:int):
         """
             Prints out a summary of the simulation
         """
-        print(str(self._workstation_total_wait_for_component_time[1]))
-        # print("\n\nSimulation Completed")
+        # print(str(self.__workstation_total_wait_for_component_time[1]))
+        print("\n\nSimulation "+str(sim_number)+" Summary")
+        
+        for i in range(3):
+            print("Component " + str(i+1))
+            print("Total blocked time : " + str(self.__component_total_block_time[i+1]))
+            print("Inspected component blocked by full buffer rate : " + str(self.__component_block_counts[i+1] / self.__component_inspection_counts[i+1]) + " from " + str(self.__component_block_counts[i+1]) +"/"+str(self.__component_inspection_counts[i+1]) + "inspections")
+            
+            if self.__component_block_counts[i+1] != 0:
+                print("Average blocked time : " + str(self.__component_total_block_time[i+1]/self.__component_block_counts[i+1]))
+            else:
+                print("Component buffering was never blocked by full buffers")
+            print()
+
+
+        print("All Component Total Block Time : " + str(sum(self.__component_total_block_time[1:])))
         # for i in range(3):
-        #     print("Component " + str(i+1) + " - Total blocked time : " + str(self._component_total_block_time[i+1]))
-        # print("Total Component Block Time : " + str(sum(self._component_total_block_time[1:])))
-        # for i in range(3):
-        #     print("Workstation " + str(i+1) + " - Total wait time : " + str(self._workstation_total_wait_for_component_time[i+1]))
-        # print("Total Workstation Wait Time : " + str(sum(self._workstation_total_wait_for_component_time[1:])))
-        # for i in range(3):
-        #     print("Product " + str(i+1) + " - Total created : " + str(self._product_counts[i+1]))
-        # print("Total products created : " + str(self._product_counts[0]))
+        #     print("Workstation " + str(i+1) + " - Total wait time : " + str(self.__workstation_total_wait_for_component_time[i+1]))
+        # print("Total Workstation Wait Time : " + str(sum(self.__workstation_total_wait_for_component_time[1:])))
+        for i in range(3):
+            print("Product " + str(i+1) + " - Total created : " + str(self.product_counts[i+1]))
+        print("Total products created : " + str(self.product_counts[0]))
+    
+    def get_average_component_block_time(self):
+        """
+            Returns a list of component specific block times
+        """
+        block_times = []
+        
+        for i in range(3):
+            if self.__component_block_counts[i+1] != 0:
+                block_times.append(self.__component_total_block_time[i+1]/self.__component_block_counts[i+1])
+            else:
+                block_times.append(0)
+        return block_times
+
+
+    
 
 
 # Main script
 if __name__ == "__main__":
-    for i in range(25):
+    component_specific_block_times = [[],[],[]]
+    for i in range(1,SIMULATION_REPLICATION_COUNT+1):
         # set seed for random number generator
         seed = 0
         if USER_CHOOSES_SEED:
@@ -217,13 +258,13 @@ if __name__ == "__main__":
         sim.start_inspection_schedule_add_to_buffer(1)
         sim.start_inspection_schedule_add_to_buffer(2)
 
-        while sim._product_counts[0] <= TOTAL_PRODUCT_CREATION_LIMIT:
+        while sim.product_counts[0] <= TOTAL_PRODUCT_CREATION_LIMIT:
             # Get next event
             # Event Tuple Structure ( time, event_type, Product||Component||InspectorNumber )
             evt = sim.get_next_chronological_event()
             # print(evt)
             # update clock
-            sim._clock = evt[0]
+            sim.clock = evt[0]
 
             #Update event type discernation
             if evt[1] == Event_Types.Inspection_Complete:
@@ -235,4 +276,11 @@ if __name__ == "__main__":
             elif evt[1] == Event_Types.Start_Next_Inspection:
                 sim.start_inspection_schedule_add_to_buffer(evt[2])
 
-        sim.print_sim_summary()
+        # sim.print_sim_summary(i)
+        current_sim_average_block_times = sim.get_average_component_block_time()
+        component_specific_block_times[0].append(current_sim_average_block_times[0])
+        component_specific_block_times[1].append(current_sim_average_block_times[1])
+        component_specific_block_times[2].append(current_sim_average_block_times[2])
+    for i in range(3):
+        print("Component #"+str(i+1))
+        print(component_specific_block_times[i])
